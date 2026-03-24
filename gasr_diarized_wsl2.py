c4f71cc9a5c8a4dbb030ae0f413b38a58b43d08e
Phương Nguyễn <ngtrphuong@gmail.com>
2026-03-25 05:48:58 +0700

    Create gasr_diarized_wsl2.py WSL2-optimized GASR

---
+++ gasr_diarized_wsl2.py
@@ -0,0 +1,26 @@
+# WSL2-Optimized GASR
+
+This script implements a WSL2-optimized version of the GASR (Generalized Adaptive Signal Reconstruction) algorithm. It is designed to perform efficiently on Windows Subsystem for Linux 2, leveraging the unique features of the environment.
+
+## Installation
+To install the necessary dependencies, run:
+
+```bash
+pip install numpy scipy
+```
+
+## Usage
+To use this script, execute:
+
+```bash
+python gasr_diarized_wsl2.py
+```
+
+## Example
+```python
+if __name__ == '__main__':
+    print("Running WSL2-optimized GASR")
+```
+
+## License
+This project is licensed under the MIT License.
